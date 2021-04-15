c0e90ba5deb902a2a7ffff9e85d0157afd6b127b
Naser Gholami <80551568+Na3r94@users.noreply.github.com>
2021-04-15 19:45:23 +0430

    Update S6-Matris.py

--- S6-Matris.py
+++ S6-Matris.py
@@ -1,21 +1,20 @@
 import random
 
 def matrix(a,b):
-    mat = [[random.randint(0,100) for i in range(a)]for j in range(b)]
+    mat = [[random.randint(0,10) for i in range(a)]for j in range(b)]
     return mat
 
 k = int(input('Enter K :'))
-n = int(input('Enter N'))
-m = int(input('Enter M'))
+n = int(input('Enter N :'))
+m = int(input('Enter M :'))
 
 Matrix1 = matrix(m,n)
 Matrix2 = matrix(k,m)
 Matrix3=[[0 for i in range(k)]for j in range(n)]
 
-
 for i in range(n):
     for j in range(k):
-        for z in range(k):
-            Matrix3[i][j]+=(Matrix1[i][j]*Matrix2[z][j])        
+        for z in range(m):
+            Matrix3[i][j]+=(Matrix1[i][z]*Matrix2[z][j])        
         
 print(Matrix3)
